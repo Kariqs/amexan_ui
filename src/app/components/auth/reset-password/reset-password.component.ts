2865dbf4a02aa2649cada1bdf4128e4fcf06e052
Kariqs <kariukibenard189@gmail.com>
2025-05-16 12:55:48 +0300

    Finished the entire auth flow.

--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -6,7 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
 
@@ -16,4 +16,90 @@ import { CommonModule } from '@angular/common';
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css',
 })
-export class ResetPasswordComponent {}
+export class ResetPasswordComponent {
+  resetPasswordForm!: FormGroup;
+  submitted = false;
+  showPassword = false;
+  showConfirmPassword = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private toaster: ToastrService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.resetPasswordForm = this.formBuilder.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: this.mustMatch('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  // Convenience getter for easy access to form fields
+  get f() {
+    return this.resetPasswordForm.controls;
+  }
+
+  togglePasswordVisibility(field: string) {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.resetPasswordForm.invalid) {
+      return;
+    }
+
+    const signupData = { ...this.resetPasswordForm.value };
+    delete signupData.confirmPassword;
+
+    const token = this.route.snapshot.queryParamMap.get('token');
+    if (!token) {
+      return;
+    } else {
+      this.authService.resetPassword(token, signupData).subscribe({
+        next: (response) => {
+          if (response) {
+            this.router.navigate(['auth', 'login']).then(() => {
+              this.toaster.success('Password has been reset successfully.');
+            });
+          } else {
+            this.toaster.error('An unknown error occured.');
+          }
+        },
+        error: (err) => {
+          this.toaster.error(err.message);
+        },
+      });
+    }
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+}
