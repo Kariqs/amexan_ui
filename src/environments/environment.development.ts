3d112dd48c6ac541a21897d0955b77b2b084d92b
Kariqs <kariukibenard189@gmail.com>
2025-12-05 14:44:32 +0300

    chenged backend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://amexan-api-production.up.railway.app',
+  apiUrl: 'https://api.freeridgehub.co.ke',
 };
